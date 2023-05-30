620695206b4899d096f88508a806f8793dc5d07a
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-30 23:42:01 +0200

    static sprite object

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from game_map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
 
 class Game:
    # in constructor we initialize the pygame modules
@@ -23,10 +24,12 @@ class Game:
       self.player = Player(self)
       self.object_renderer = ObjectRenderer(self)
       self.raycasting = RayCasting(self)
+      self.static_sprite = SpriteObject(self)
    
    def update(self):
       self.player.update()
       self.raycasting.update()
+      self.static_sprite.update()
       pg.display.flip()
       self.delta_time = self.clock.tick(FPS)
       pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
